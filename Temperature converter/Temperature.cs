f7a14898a2bc55a8196b949ee4d86ad815d9b60a
jaguar48 <okonkwostanley67@yahoo.com>
2023-01-16 20:42:31 +0100

    Add project files.

---
+++ Temperature converter/Temperature.cs
@@ -0,0 +1,10 @@
+﻿namespace Temperature_converter
+{
+    public class Temperature
+    {
+        public static double CelsiusToFahrenheit(double celsius) => (celsius * 9 / 5) + 32;
+
+        public static double FahrenheitToCelsius(double fahrenheit) => (fahrenheit - 32) * 5 / 9;
+
+    }
+}
